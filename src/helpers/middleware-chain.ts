de2af5701e47fde0702289da3d46fb3f3a084fb0
zishone <zishgarces@gmail.com>
2019-09-12 22:17:22 +0800

    Removed bluebird dependency

--- src/helpers/middleware-chain.ts
+++ src/helpers/middleware-chain.ts
@@ -1,10 +1,3 @@
-import bluebird = require('bluebird');
-import {
-  NextFunction,
-  Request,
-  Response,
-} from 'express';
-
 export class MiddlewareChain {
   private middlewares: any[];
 
@@ -13,25 +6,10 @@ export class MiddlewareChain {
   }
 
   public getHandler(): any {
-    return (req: Request, res: Response, next: NextFunction) => {
-      bluebird
-        .map(this.middlewares, async (middleware) => {
-          await new Promise((resolve, reject) => {
-            middleware(req, res, (error: Error) => {
-              if (error) {
-                reject(error);
-              } else {
-                resolve();
-              }
-            });
-          });
-        }, { concurrency: 1 })
-        .then(() => {
-          next();
-        })
-        .catch((error: Error) => {
-          next(error);
-        });
+    return (...args: any[]) => {
+      const chain = this.middlewares.concat((..._args: any[]) => _args.slice(0, args.length));
+      const next = (..._args: any[]) => chain.shift()(...(_args.length > 0 ? _args : args), next);
+      return next();
     };
   }
 }
