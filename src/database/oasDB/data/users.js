278f7cb20e5efbc5bcddaa515ce4d35c30cb0f5f
zishone <zishgarces@gmail.com>
2020-04-10 22:38:03 +0800

    Add db migration with mongover

---
+++ src/database/oasDB/data/users.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    _id: {
+      $oid: '5e8f1fdec909963f71353071',
+    },
+    username: 'string',
+    password: '$2a$12$GWLLEuXtmL.K07I1MWquA.cZoibCuhxOJcp9D/sw1N9XGd.2SxLPu',
+    createDate: + new Date(),
+  }
+]
