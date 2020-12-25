b1489471dfa001eb43d0ada09519cde34ead910b
zishone <zishgarces@gmail.com>
2020-12-25 20:50:52 +0100

    Attempt one by using root flag

--- tests/integration/routes/index.test.ts
+++ tests/integration/routes/index.test.ts
@@ -1,34 +1,6 @@
-import { before, describe } from 'mocha';
+import { describe } from 'mocha';
 import { health } from './health/health.test';
-import { mongo } from '../../../src/server';
 
 describe('routes', (): void => {
-  before(
-    'before',
-    async (): Promise<void> => {
-      const retries = 10;
-      const connect = async (retry: number = 0): Promise<void> => {
-        try {
-          await mongo.getDb();
-        } catch (_error) {
-          if (retry > retries) {
-            return;
-          }
-          // eslint-disable-next-line no-unused-vars
-          await connect(retry + 1);
-        }
-      };
-      await connect();
-    },
-  );
-
-  after(
-    'after',
-    async (): Promise<void> => {
-      const db = await mongo.getDb();
-      db.dropDatabase();
-    },
-  );
-
   describe('/health', health);
 });
