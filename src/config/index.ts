4861660a5f4645fbbb7fc55175dfa694104463a8
zishone <zishgarces@gmail.com>
2020-12-25 10:42:20 +0100

    Update dotenv-default for much cleaner defaulting of config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,4 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
+import 'dotenv-defaults/config';
 
 const { NODE_ENV, CONFIG_APP_PORT, CONFIG_DB_URI, CONFIG_DB_NAME, CONFIG_LOGIN_SECRET, CONFIG_LOGIN_TTL } = process.env;
 
@@ -8,13 +6,13 @@ export const config = {
   APP_NAME: require('../../package.json').name,
   APP_VERSION: require('../../package.json').version,
 
-  ENV: NODE_ENV || 'development',
+  ENV: NODE_ENV,
 
-  APP_PORT: parseFloat(CONFIG_APP_PORT || '3000'),
+  APP_PORT: parseFloat(CONFIG_APP_PORT),
 
-  DB_URI: CONFIG_DB_URI || 'mongodb://127.0.0.1:27017',
-  DB_NAME: CONFIG_DB_NAME || 'oasDB',
+  DB_URI: CONFIG_DB_URI,
+  DB_NAME: CONFIG_DB_NAME,
 
-  LOGIN_SECRET: CONFIG_LOGIN_SECRET || 'login_secret',
-  LOGIN_TTL: parseFloat(CONFIG_LOGIN_TTL || '2592000'),
+  LOGIN_SECRET: CONFIG_LOGIN_SECRET,
+  LOGIN_TTL: parseFloat(CONFIG_LOGIN_TTL),
 };
