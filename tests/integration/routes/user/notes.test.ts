058efbccbcd4ca47caaf3d59191223f19c266cc3
zishone <zishgarces@gmail.com>
2020-12-27 22:13:22 +0100

    Add more coverage to integration tests

--- tests/integration/routes/user/notes.test.ts
+++ tests/integration/routes/user/notes.test.ts
@@ -2,15 +2,23 @@ import { NoteModel, UserModel } from '../../../../src/models';
 import { app, logger, mongo } from '../../../../src/server';
 import { describe, it } from 'mocha';
 import { expect, request, use } from 'chai';
+import { NoteService } from '../../../../src/services';
 import { ROLES } from '../../../../src/constants';
 import chaiHttp from 'chai-http';
 import { config } from '../../../../src/config';
+import { createSandbox } from 'sinon';
 import { nanoid } from 'nanoid';
 import { sign } from 'jsonwebtoken';
 
 use(chaiHttp);
 
 export const userNotes = (): void => {
+  const sandbox = createSandbox();
+
+  afterEach((): void => {
+    sandbox.restore();
+  });
+
   describe('POST', (): void => {
     it('should respond 201', async (): Promise<void> => {
       const testUser = {
@@ -38,6 +46,36 @@ export const userNotes = (): void => {
         .send(testNote);
 
       expect(response.status).to.be.equal(201);
+    });
+
+    it('should respond 500 WHEN unknown error occurs', async (): Promise<void> => {
+      const testUser = {
+        username: nanoid(12),
+        email: nanoid(12),
+        password: nanoid(12),
+        name: nanoid(12),
+        role: ROLES.USER,
+        createdOn: Date.now(),
+      };
+      const userModel = new UserModel(logger, mongo);
+      const [testUserId] = await userModel.save(testUser);
+      const testAccessToken = sign({ id: testUserId }, config.LOGIN_SECRET, { expiresIn: config.LOGIN_TTL });
+      const testNote = {
+        userId: testUserId,
+        title: nanoid(12),
+        body: nanoid(12),
+        modifiedOn: Date.now(),
+        createdOn: Date.now(),
+      };
+
+      sandbox.stub(NoteService.prototype, 'createUserNote').onCall(0).rejects();
+
+      const response = await request(app)
+        .post('/api/v1/user/notes')
+        .set('Authorization', `Bearer ${testAccessToken}`)
+        .send(testNote);
+
+      expect(response.status).to.be.equal(500);
     });
   });
 
@@ -70,6 +108,29 @@ export const userNotes = (): void => {
         .send();
 
       expect(response.status).to.be.equal(200);
+    });
+
+    it('should respond 500 WHEN unknown error occurs', async (): Promise<void> => {
+      const testUser = {
+        username: nanoid(12),
+        email: nanoid(12),
+        password: nanoid(12),
+        name: nanoid(12),
+        role: ROLES.USER,
+        createdOn: Date.now(),
+      };
+      const userModel = new UserModel(logger, mongo);
+      const [testUserId] = await userModel.save(testUser);
+      const testAccessToken = sign({ id: testUserId }, config.LOGIN_SECRET, { expiresIn: config.LOGIN_TTL });
+
+      sandbox.stub(NoteService.prototype, 'fetchUserNotes').onCall(0).rejects();
+
+      const response = await request(app)
+        .get('/api/v1/user/notes')
+        .set('Authorization', `Bearer ${testAccessToken}`)
+        .send();
+
+      expect(response.status).to.be.equal(500);
     });
   });
 };
--- tests/integration/routes/users/users.test.ts
+++ tests/integration/routes/users/users.test.ts
@@ -3,14 +3,22 @@ import { describe, it } from 'mocha';
 import { expect, request, use } from 'chai';
 import { ROLES } from '../../../../src/constants';
 import { UserModel } from '../../../../src/models';
+import { UserService } from '../../../../src/services';
 import chaiHttp from 'chai-http';
 import { config } from '../../../../src/config';
+import { createSandbox } from 'sinon';
 import { nanoid } from 'nanoid';
 import { sign } from 'jsonwebtoken';
 
 use(chaiHttp);
 
 export const users = (): void => {
+  const sandbox = createSandbox();
+
+  afterEach((): void => {
+    sandbox.restore();
+  });
+
   describe('GET', (): void => {
     it('should respond 200', async (): Promise<void> => {
       const testAdmin = {
@@ -168,6 +176,29 @@ export const users = (): void => {
         .send();
 
       expect(response.status).to.be.equal(400);
+    });
+
+    it('should respond 500 WHEN unknown error occurs', async (): Promise<void> => {
+      const testAdmin = {
+        username: nanoid(12),
+        email: nanoid(12),
+        password: nanoid(12),
+        name: nanoid(12),
+        role: ROLES.ADMIN,
+        createdOn: Date.now(),
+      };
+      const userModel = new UserModel(logger, mongo);
+      const [testAdminId] = await userModel.save(testAdmin);
+      const testAccessToken = sign({ id: testAdminId }, config.LOGIN_SECRET, { expiresIn: config.LOGIN_TTL });
+
+      sandbox.stub(UserService.prototype, 'fetchUsers').onCall(0).rejects();
+
+      const response = await request(app)
+        .get('/api/v1/users?fields=username&sort=createdOn&limit=0&skip=0&page=1')
+        .set('Authorization', `Bearer ${testAccessToken}`)
+        .send();
+
+      expect(response.status).to.be.equal(500);
     });
   });
 };
