ba36b5560abcb27d84f378ef3892882abff880b5
zishone <zishgarces@gmail.com>
2021-01-06 11:06:40 +0100

    Fix socket integration test

---
+++ tests/integration/sockets/connect.test.ts
@@ -0,0 +1,41 @@
+import { EVENTS, ROLES } from '../../../src/constants';
+import { logger, mongo } from '../../../src/server';
+import { UserModel } from '../../../src/models';
+import { config } from '../../../src/config';
+import { expect } from 'chai';
+import { it } from 'mocha';
+import { nanoid } from 'nanoid';
+import { sign } from 'jsonwebtoken';
+import socket from 'socket.io-client';
+
+export const connect = (): void => {
+  it('should emit connect_error when token is invalid', async (): Promise<void> => {
+    const testUser = {
+      username: nanoid(12),
+      email: nanoid(12),
+      password: nanoid(12),
+      name: nanoid(12),
+      role: ROLES.USER,
+      createdOn: Date.now(),
+    };
+    const userModel = new UserModel(logger, mongo);
+    const [testUserId] = await userModel.save(testUser);
+    const testUserAccessToken = sign({ id: testUserId }, config.LOGIN_SECRET, { expiresIn: config.LOGIN_TTL });
+
+    const io = socket.connect(`http://localhost:${config.APP_PORT}`, {
+      query: { token: testUserAccessToken },
+      transports: ['websocket'],
+    } as any);
+
+    let isConnected = false;
+    io.on(EVENTS.CONNECT, (): void => {
+      isConnected = true;
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 100));
+
+    io.disconnect();
+
+    expect(isConnected).to.be.equal(true);
+  });
+};
--- tests/integration/sockets/index.test.ts
+++ tests/integration/sockets/index.test.ts
@@ -3,14 +3,14 @@ import { afterEach, beforeEach, describe } from 'mocha';
 import { logger, mongo } from '../../../src/server';
 import { EVENTS } from '../../../src/constants';
 import chaiHttp from 'chai-http';
+import { connect } from './connect.test';
 import { connectError } from './connect-error.test';
 import { migrate } from '../../../src/utils';
-import { notification } from './notification.test';
 import { use } from 'chai';
 
 use(chaiHttp);
 
-describe.only('sockets', (): void => {
+describe('sockets', (): void => {
   beforeEach(
     async (): Promise<void> => {
       await migrate(logger, mongo);
@@ -24,6 +24,6 @@ describe.only('sockets', (): void => {
     },
   );
 
-  describe(EVENTS.NOTIFICATION, notification);
+  describe(EVENTS.CONNECT, connect);
   describe(EVENTS.CONNECT_ERROR, connectError);
 });
