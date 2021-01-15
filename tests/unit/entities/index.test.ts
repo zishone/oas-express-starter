7b10b0d6a87b2d13f969c78b65913c95bd2ad288
zishone <zishgarces@gmail.com>
2021-01-15 23:02:37 +0100

    Remove entities tests

--- tests/unit/entities/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe } from 'mocha';
-import note from './note.test';
-import user from './user.test';
-
-describe('models', (): void => {
-  describe('NoteModel', note);
-  describe('UserModel', user);
-});
