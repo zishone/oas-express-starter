64ebf5a18a0e466b5650baf2c68fc0a7684e5f5d
zishone <zishgarces@gmail.com>
2021-02-15 18:07:35 +0100

    Add hostname and env fields to logs

--- src/configs/env.config.ts
+++ src/configs/env.config.ts
@@ -1,9 +1,11 @@
 import 'dotenv-defaults/config';
+import { hostname } from 'os';
 
 const { NODE_ENV, CONFIG_APP_PORT, CONFIG_DB_URI, CONFIG_DB_NAME, CONFIG_LOGIN_SECRET, CONFIG_LOGIN_TTL } = process.env;
 
 export const envConfig = {
   ENV: NODE_ENV,
+  HOSTNAME: hostname(),
 
   APP_PORT: parseFloat(CONFIG_APP_PORT),
 
