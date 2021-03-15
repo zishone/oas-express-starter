476979a2e2dea26ecd3428df6fe41702a938134d
zishone <zishgarces@gmail.com>
2021-03-15 19:07:00 +0100

    Move passport strategy configuration to app.ts

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,6 +1,6 @@
 export * from './error.middleware';
 export * from './log.middleware';
-export * from './passport.middleware';
+export * from './authentication.middleware';
 export * from './request-id.middleware';
 export * from './role.middleware';
 export * from './upload.middleware';
